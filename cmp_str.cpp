26aae63bddd6b28720bc05b48d9493664762f329
No4ovka <92082996+No4ovka@users.noreply.github.com>
2021-11-11 12:05:21 +0300

    Add files via upload

--- cmp_str.cpp
+++ cmp_str.cpp
@@ -2,10 +2,12 @@
 
 string itc_cmp_str(string str1, string str2, int num){
     string str3;
-    int m=0;
-    for (int k=0;k<itc_len(str1);k++){
-        if (k<num)
+    long long m=0, k = 0;
+    while (k<itc_len(str1){
+        if (k<num){
             str3=str3+str1[k];
+            k++;
+        }
         else if (k>=num){
             if (m<itc_len(str2)){
                 str3=str3+str2[m];
@@ -17,3 +19,4 @@ string itc_cmp_str(string str1, string str2, int num){
     }
     return str3;
 }
+
