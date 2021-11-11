84817c99aa8ae4fc157552534267e124f8cbc423
No4ovka <92082996+No4ovka@users.noreply.github.com>
2021-11-11 11:30:56 +0300

    Add files via upload

--- reverse.cpp
+++ reverse.cpp
@@ -1,7 +1,7 @@
 #include "str_easy.h"
 
 string itc_reverse_str(string str){
-    long long i = itc_len(str), k=0;
+    long long i = itc_len(str) - 1, k=0;
     string rev = "";
     while (k<itc_len(str)){
         rev+=str[i];
