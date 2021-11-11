ba729061510fcd525e3b0b9d483249c402972e02
No4ovka <92082996+No4ovka@users.noreply.github.com>
2021-11-11 12:53:11 +0300

    Update cmp_str.cpp

--- cmp_str.cpp
+++ cmp_str.cpp
@@ -1,25 +1,24 @@
 #include "str_easy.h"
 
-string itc_cmp_str(string str1,string str2, int num)
-{
-    string neww="";
-    long long lstr2=itc_len(str2);
-    long long lstr1=itc_len(str1);
-    if (lstr1 < num || num < 0) return str1;
-    if(str2=="")
-    {
-        return(str1);
-    }
-    for(long long i=0;i<lstr1;i++)
-    {
-        if (i<num ) {
-            neww+=str1[i];
-        }else if (i > (num+lstr2-1)) {
-            neww+=str1[i-lstr2];
+string itc_cmp_str(string str1, string str2, int num){
+    string str3;
+    long long m=0, k = 0, schet = 0;
+    while (schet<itc_len(str1)){
+        if (k<num){
+            str3=str3+str1[k];
+            k++;
         }
-        else {
-            neww+=str2[i-num];
+        else if (k>=num){
+            if (m<itc_len(str2)){
+                str3=str3+str2[m];
+                m++;
+            }
+            else{
+                str3=str3+str1[k];
+                k++;
+            }
         }
+        schet++;
     }
-    return neww;
+    return str3;
 }
