3accc2001d6bb10e5277907a26987e7cce6dab92
No4ovka <92082996+No4ovka@users.noreply.github.com>
2021-11-11 12:26:27 +0300

    Add files via upload

--- find_str.cpp
+++ find_str.cpp
@@ -1,16 +1,17 @@
 #include "str_easy.h"
 
-int itc_find_str(string str1, string str2) {
+int itc_find_str(string str1, string str2) { // 12
     bool test = true;
-    char txt1 = 'a', txt2 = 'a';
+    char text1 = 'a';
+    char text2 = 'a';
     for (int n1 = 0; n1 < itc_len(str1); n1++) {
         if (str1[n1] == str2[0]) {
             test = true;
             if ((itc_len(str1) - (n1 + 1)) >= itc_len(str2) - 1) {
                 for (int n2 = 1; n2 < itc_len(str2); n2++) {
-                    txt1 = str1[n1 + n2];
-                    txt2 = str2[n2];
-                    if (txt1 != txt2)
+                    text1 = str1[n1 + n2];
+                    text2 = str2[n2];
+                    if (text1 != text2)
                         test = false;
                 }
                 if (test == true)
