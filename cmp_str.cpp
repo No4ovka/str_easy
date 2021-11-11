940f320b8bad6e7daba7cf9642da7670a18c380c
No4ovka <92082996+No4ovka@users.noreply.github.com>
2021-11-11 12:34:02 +0300

    Add files via upload

--- cmp_str.cpp
+++ cmp_str.cpp
@@ -1,23 +1,25 @@
 #include "str_easy.h"
 
-string itc_cmp_str(string str1, string str2, int num){
-    string str3;
-    long long m=0, k = 0, schet = 0;
-    while (schet<itc_len(str1)){
-        if (k<num){
-            str3=str3+str1[k];
-            k++;
-        }
-        else if (k>=num){
-            if (m<itc_len(str2)){
-                str3=str3+str2[m];
-                m++;
-            }
-            else
-                str3=str3+str1[k];
-        }
-        schet++;
+string itc_cmp_str(string str1,string str2, int num)
+{
+    string neww="";
+    long long lstr2=itc_len(str2);
+    long long lstr1=itc_len(str1);
+    if (lstr1 < num || num < 0) return str1;
+    if(str2=="")
+    {
+        return(str1);
     }
-    return str3;
+    for(long long i=0;i<lstr1;i++)
+    {
+        if (i<num ) {
+            neww+=str1[i];
+        }else if (i > (num+lstr2-1)) {
+            neww+=str1[i-lstr2];
+        }
+        else {
+            neww+=str2[i-num];
+        }
+    }
+    return neww;
 }
-
--- find_str.cpp
+++ find_str.cpp
@@ -1,23 +1,25 @@
 #include "str_easy.h"
 
-int itc_find_str(string str1, string str2) { // 12
-    bool test = true;
-    char text1 = 'a';
-    char text2 = 'a';
-    for (int n1 = 0; n1 < itc_len(str1); n1++) {
-        if (str1[n1] == str2[0]) {
-            test = true;
-            if ((itc_len(str1) - (n1 + 1)) >= itc_len(str2) - 1) {
-                for (int n2 = 1; n2 < itc_len(str2); n2++) {
-                    text1 = str1[n1 + n2];
-                    text2 = str2[n2];
-                    if (text1 != text2)
-                        test = false;
-                }
-                if (test == true)
-                    return n1;
+int itc_find_str(string str1,string str2)
+{
+    long long lstr1=itc_len(str1);
+    long long lstr2=itc_len(str2);
+    int k=0;
+    for(long long i=0;i<lstr1;i++)
+    {
+
+        if(str1[i]==str2[k])
+        {
+            k=k+1;
+            if(k==lstr2)
+            {
+            return i-k+1;
             }
         }
+        else
+        {
+            k=0;
+        }
     }
-	return -1;
+    return -1;
 }
