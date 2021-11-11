e2ee0ba8768ee33aa8058c28ff2041cb0f1edb14
No4ovka <92082996+No4ovka@users.noreply.github.com>
2021-11-11 11:43:27 +0300

    Update reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -1,6 +1,6 @@
 #include "str_easy.h"
 
-string itc_reverse_str(string str) { // 8
+string itc_reverse_str(string str) { 
     string rev = "";
     for (int n1 = itc_len(str); n1 > 0; n1--) {
         rev += str[n1 - 1];
